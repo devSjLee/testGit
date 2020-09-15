1e4d55ee842f5f7adaf623a4334b45e9c40afa0f
samsung <samsung@samsung-PC>
2020-09-15 11:08:45 +0900

    01_person add properties name, age -sj-

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -2,5 +2,34 @@ package com.git.model.vo;
 
 public class Person {
 //	회원정보를 저장하는 클래스
+	private String name;
+	private int age;
+	
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 }
